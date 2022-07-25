4f90a5a694e0944a9131d688b947a3dc70acbe5f
Jakob Lykke Andersen <Jakob@caput.dk>
2022-07-25 20:59:48 +0200

    Drop jQuery and general updates.

--- source/_static/redirecttools.js
+++ source/_static/redirecttools.js
@@ -1,90 +1,66 @@
 /*
  * redirecthtools.js
- * ~~~~~~~~~~~~~~~~
+ * ~~~~~~~~~~~~~~~~~
  *
- * Sphinx JavaScript utilities for redirecting to an entity based
- * given its anchor.
+ * Sphinx JavaScript utilities for redirecting to an entity given
+ * its anchor.
  * Based on searchtools.js and uses searchindex.js as data source.
  *
- * :copyright: Copyright 2021-2021 by the Sphinx team, see AUTHORS.
+ * :copyright: Copyright 2021-2022 by Jakob Lykke Andersen.
  * :license: BSD, see LICENSE for details.
  *
  */
 
+"use strict";
+
 // We load searchindex.js so we define this.setIndex to get the data.
 var Search = {
-  setIndex : function(index) {
-    Redirect.setIndex(index);
-  },
+  setIndex: (index) => Redirect.setIndex(index),
 }
 
 var Redirect = {
-  _index : null,
-  _queued_query : null,
-  _pulse_status : -1,
+  _index: null,
+  _queued_query: null,
 
-  init : function() {
-      var params = $.getQueryParameters();
-      if (params.q) {
-          var query = params.q[0];
-          this.performSearch(query);
+  init: function() {
+      const query = new URLSearchParams(window.location.search).get("q");
+      if(query) {
+          Redirect.performSearch(query);
       } else {
-          var out = $('#search-results');
-          var p = $('<p>No query ID provided. Use <code>?q=theId</code> to redirec.</p>').appendTo(out);
+          const out = document.getElementById('search-results');
+          out.innerHTML = '<p>No query ID provided. Use <code>?q=theId</code> to redirect.</p>';
       }
   },
 
-  setIndex : function(index) {
+  setIndex: function(index) {
     var q;
     this._index = index;
-    if ((q = this._queued_query) !== null) {
+    if((q = this._queued_query) !== null) {
       this._queued_query = null;
       this.query(q);
     }
   },
 
-  hasIndex : function() {
+  hasIndex: function() {
       return this._index !== null;
   },
 
-  deferQuery : function(query) {
+  deferQuery: function(query) {
       this._queued_query = query;
-  },
-
-  stopPulse : function() {
-      this._pulse_status = 0;
-  },
-
-  startPulse : function() {
-    if (this._pulse_status >= 0)
-        return;
-    function pulse() {
-      var i;
-      this._pulse_status = (this._pulse_status + 1) % 4;
-      var dotString = '';
-      for (i = 0; i < this._pulse_status; i++)
-        dotString += '.';
-      Redirect.dots.text(dotString);
-      if (this._pulse_status > -1)
-        window.setTimeout(pulse, 500);
-    }
-    pulse();
   },
 
   /**
    * perform a search for ID (or wait until index is loaded)
    */
-  performSearch : function(query) {
+  performSearch: function(query) {
     // create the required interface elements
-    this.out = $('#search-results');
-    this.title = $('<h2>' + _('Searching') + '</h2>').appendTo(this.out);
-    this.dots = $('<span></span>').appendTo(this.title);
-
-    $('#search-progress').text(_('Preparing search...'));
-    this.startPulse();
+    this.out = document.getElementById('search-results');
+    this.title = document.createElement('h2');
+	this.out.appendChild(this.title);
+    this.title.innerHTML = _('Searching') + '...';
 
     // index already loaded, the browser was quick!
-    if (this.hasIndex())
+    if(this.hasIndex())
       this.query(query);
     else
       this.deferQuery(query);
@@ -93,64 +69,60 @@ var Redirect = {
   /**
    * execute search (requires search index to be loaded)
    */
-  query : function(query) {
+  query: function(query) {
     var objects = this._index.objects;
     var result = Redirect.performObjectSearch(query);
-      if (result) {
-        var requestUrl = "";
-        var linkUrl = "";
-        if (DOCUMENTATION_OPTIONS.BUILDER === 'dirhtml') {
-          // dirhtml builder
-          var dirname = result[0] + '/';
-          if (dirname.match(/\/index\/$/)) {
-            dirname = dirname.substring(0, dirname.length-6);
-          } else if (dirname == 'index/') {
-            dirname = '';
-          }
-          requestUrl = DOCUMENTATION_OPTIONS.URL_ROOT + dirname;
-          linkUrl = requestUrl;
-        } else {
-          // normal html builders
-          requestUrl = DOCUMENTATION_OPTIONS.URL_ROOT + result[0] + DOCUMENTATION_OPTIONS.FILE_SUFFIX;
-          linkUrl = result[0] + DOCUMENTATION_OPTIONS.LINK_SUFFIX;
+    if(result) {
+      var requestUrl = "";
+      var linkUrl = "";
+      if(DOCUMENTATION_OPTIONS.BUILDER === 'dirhtml') {
+        // dirhtml builder
+        var dirname = result[0] + '/';
+        if(dirname.match(/\/index\/$/)) {
+          dirname = dirname.substring(0, dirname.length-6);
+        } else if(dirname == 'index/') {
+          dirname = '';
         }
-        var fullUrl = linkUrl + result[1];
-        window.location.replace(fullUrl);
+        requestUrl = DOCUMENTATION_OPTIONS.URL_ROOT + dirname;
+        linkUrl = requestUrl;
       } else {
-        Redirect.stopPulse();
-        Redirect.title.text(_('ID not found') + ": " + query);
+        // normal html builders
+        requestUrl = DOCUMENTATION_OPTIONS.URL_ROOT + result[0] + DOCUMENTATION_OPTIONS.FILE_SUFFIX;
+        linkUrl = result[0] + DOCUMENTATION_OPTIONS.LINK_SUFFIX;
       }
+      var fullUrl = linkUrl + result[1];
+      window.location.replace(fullUrl);
+    } else {
+      this.title.innerHTML = _('ID not found') + ": " + query;
+    }
   },
 
   /**
    * search for object names
    */
-  performObjectSearch : function(query) {
+  performObjectSearch: function(query) {
     var docnames = this._index.docnames;
     var objects = this._index.objects;
     var objnames = this._index.objnames;
 
-    for (var prefix in objects) {
-       for (var i = 0; i != objects[prefix].length; ++i) {
-          var entry = objects[prefix][i];
-          var name = entry[4];
-          var fullname = (prefix ? prefix + '.' : '') + name;
-          var objname = objnames[entry[1]][2];
-          var anchor = entry[3];
-          if (anchor === '')
-            anchor = fullname;
-          else if (anchor == '-')
-            anchor = objnames[entry[1]][1] + '-' + fullname;
-            fullname, objname, anchor)
-          if (anchor == query) {
-           return [docnames[entry[0]], '#' + anchor];
-          }
+    for(var prefix in objects) {
+      for(var i = 0; i != objects[prefix].length; ++i) {
+        var entry = objects[prefix][i];
+        var name = entry[4];
+        var fullname = (prefix ? prefix + '.' : '') + name;
+        var objname = objnames[entry[1]][2];
+        var anchor = entry[3];
+        if(anchor === '')
+          anchor = fullname;
+        else if(anchor == '-')
+          anchor = objnames[entry[1]][1] + '-' + fullname;
+        if(anchor == query) {
+         return [docnames[entry[0]], '#' + anchor];
+        }
       }
     }
     return null;
   },
 };
 
-$(document).ready(function() {
-  Redirect.init();
-});
+_ready(Redirect.init);
