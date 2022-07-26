dff431cac000daf172d2f28edb7ef026398349ed
Jakob Lykke Andersen <Jakob@caput.dk>
2022-07-26 14:19:57 +0200

    Rearrange files

---
+++ example/source/conf.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+# this should probably be 'extensions/' or a similar subdir
+sys.path.append(os.path.abspath('../../'))
+extensions = [
+	'redirect_by_id',
+]
--- source/conf.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-sys.path.append(os.path.abspath('extensions'))
-extensions = [
-	'redirect_by_id',
-]
